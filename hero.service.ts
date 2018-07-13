eb4a0689dbe3b13a83769fbc09ef3b34b030e43c
Your Name <you@example.com>
2018-07-13 11:34:36 +0530

    http

--- hero.service.ts
+++ hero.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import { catchError, map, tap } from "rxjs/operators";
 import { Observable, of } from 'rxjs';
-
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
-
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
-
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+// import { ErrorComponent } from "./error/error.component";
+
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
